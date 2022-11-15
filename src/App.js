98cfb6ec63cb919b839033a7137a150b938e6257
Ömer Sakmar <omersakmar@gmail.com>
2022-11-15 12:32:18 +0300

    create project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [points, setPoints] = useState([]);
+  const [popped, setPopped] = useState([]);
+
+  const handlePlaceCircle = (e) => {
+    const { clientX, clientY } = e;
+    setPoints([...points, { x: clientX, y: clientY }]);
+    console.log(points);
+  };
+  const handleUndo = () => {
+    const newPoints = [...points];
+    const poppedPoint = newPoints.pop();
+    if (!poppedPoint) return;
+    setPopped([...popped, poppedPoint]);
+    setPoints(newPoints);
+  };
+  const handleRedo = () => {
+    const newPopped = [...popped];
+    const poppedPoint = newPopped.pop();
+    if (!poppedPoint) return;
+    setPoints([...points, poppedPoint]);
+    setPopped(newPopped);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <div className="buttonContainer">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Clicking somewhere on this page will create a circle. You can use
+          these buttons to perform some functions.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className="buttons">
+          <button disabled={points.length === 0} onClick={handleUndo}>
+            Undo
+          </button>
+          <button disabled={popped.length === 0} onClick={handleRedo}>
+            Redo
+          </button>
+        </div>
+      </div>
+      <div className="App" onClick={handlePlaceCircle}>
+        {points.map((point, index) => (
+          <div
+            key={index}
+            className="point"
+            style={{ left: point.x - 5 + "px", top: point.y - 5 + "px" }}
+          ></div>
+        ))}
+      </div>
+    </>
   );
 }
 
